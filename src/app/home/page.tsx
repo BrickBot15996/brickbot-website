02ed54217b002562a62629f37a4cc47552182db2
mariusbaican <155394478+mariusbaican@users.noreply.github.com>
2025-04-22 17:40:25 +0300

    Updated some scaling

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,9 +3,12 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div lang="en">
-      <header className="absolute top-0 left-0 w-full h-[63px] bg-[#121212] z-50 flex justify-center text-[#ffffff] font-bold text-[18px]">
-        <div className="flex space-x-10 items-center">
+    <div
+      lang="en"
+      className="overflow-x-hidden"
+    >
+      <header className="absolute top-0 left-0 w-full bg-[#121212] z-50 flex justify-center text-[#ffffff] font-bold text-[clamp(0.5rem,_5vw,_1.25rem)] h-[63px]">
+        <div className="flex justify-center items-center w-full max-w-screen-xl px-4 mx-auto space-x-8">
           <Link
             href="/tbd"
             className="hover:opacity-75"
@@ -27,7 +30,7 @@ export default function Home() {
               alt="BrickBot Logo"
               width={928}
               height={502}
-              className="w-[140px] h-[76px] object-cover mt-[10px]"
+              className="w-[140px] h-[76px] object-contain mt-[10px]"
             />
           </Link>
           <Link
@@ -44,6 +47,7 @@ export default function Home() {
           </Link>
         </div>
       </header>
+
       <main>
         <div>
           <Image
@@ -51,29 +55,25 @@ export default function Home() {
             alt="random"
             height={1280}
             width={1920}
-            className="absolute top-[63px] right-[0px] w-[1770px] h-[600px] object-cover z-[-1] opacity-50"
+            className="absolute top-[63px] right-0 w-full h-[600px] object-cover z-[-1] opacity-50"
           />
         </div>
-        <div className="absolute top-[63px] left-0 w-[1770px] h-[600px] bg-[linear-gradient(70deg,_#000000_30%,_transparent)] z-10">
-          <div className="relative z-20 text-[#ffd100] text-[50px] font-extrabold top-[30%] left-[5%]">
+
+        <div className="absolute top-[63px] left-0 w-full h-[600px] bg-[linear-gradient(70deg,_#000000_30%,_transparent)] z-10">
+          <div className="relative z-20 text-[#ffd100] text-[clamp(1.5rem,_5vw,_3rem)] font-extrabold top-[30%] left-[5%] px-4">
             <p className="mt-[-10px] mb-[-10px]">We build our future</p>
             <p className="mt-[-10px] mb-[-10px]">Brick by Brick!</p>
             <Link href="/support-us">
-              <button className="flex items-center justify-center h-[50px] w-[250px] bg-[linear-gradient(180deg,_#7D6800_0%,_#413400_50%)] text-[#ffd100] font-extrabold text-[18px] rounded-[25px] hover:opacity-75 border-2 border-[#ffd100]">
+              <button className="flex items-center justify-center h-[clamp(20px,_10vw,_50px)] w-[clamp(100px,_40vw,_200px)] max-w-full bg-[linear-gradient(180deg,_#7D6800_0%,_#413400_50%)] text-[#ffd100] font-extrabold text-[clamp(0.5rem,_5vw,_1.1rem)] rounded-[clamp(10px,_10vw,_25px)] hover:opacity-75 border-2 border-[#ffd100] mt-[20px]">
                 BECOME A SPONSOR
-                <Image
-                  src="/right-arrow.png"
-                  alt="arrow"
-                  width={25}
-                  height={10}
-                  className="ml-[8px]"
-                />
               </button>
             </Link>
           </div>
         </div>
-        <div className="relative top-[670px] left-0 w-full h-[2px] bg-[linear-gradient(90deg,_#000000_10%,_#ffd100_35%,_#000000_85%)] opacity-50 z-10"></div>
+
+        <div className="absolute top-[663px] left-0 w-full h-[2px] bg-[linear-gradient(90deg,_#000000_10%,_#ffd100_35%,_#000000_85%)] opacity-50 z-10"></div>
       </main>
+
       <footer>{/* Your footer content goes here */}</footer>
     </div>
   );
