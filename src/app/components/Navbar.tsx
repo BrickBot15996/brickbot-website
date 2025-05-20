829844eaa765ddf5e88a2e8ad5260069e8413d99
mariusbaican <155394478+mariusbaican@users.noreply.github.com>
2025-05-20 22:34:22 +0300

    Animations 🆒

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,41 +3,42 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="h-[4vw] bg-[#121212] z-50 flex justify-center items-center text-[#ffffff] font-bold text-[clamp(0px,_2vw,24px)]">
+    <div className="h-[6vh] bg-[#121212] flex justify-center items-center text-[#ffffff] font-bold text-[2vh] overflow-visible">
       <div className="flex justify-center items-center w-full max-w-screen-xl px-4 mx-auto space-x-[3vw]">
         <Link
           href="/tbd"
-          className="hover:opacity-75"
+          className="hover:opacity-75 hover:scale-110 transition-transform duration-300"
         >
           TBD
         </Link>
         <Link
           href="/achievements"
-          className="hover:opacity-75"
+          className="hover:opacity-75 hover:scale-110 transition-transform duration-300"
         >
           ACHIEVEMENTS
         </Link>
         <Link
           href="/home"
-          className="hover:opacity-75"
+          className="relative z-10 hover:opacity-75 hover:scale-110 transition-transform duration-300"
         >
           <Image
             src="/brick_yellow.png"
             alt="BrickBot Logo"
+            priority
             width={928}
             height={502}
-            className="w-[clamp(0px,_10vw,_180px)] h-auto object-contain mt-[2vw]"
+            className="h-[8vh] w-auto object-contain -mb-[1.5vh]"
           />
         </Link>
         <Link
           href="/resources"
-          className="hover:opacity-75"
+          className="hover:opacity-75 hover:scale-110 transition-transform duration-300"
         >
           RESOURCES
         </Link>
         <Link
           href="/support-us"
-          className="hover:opacity-75"
+          className="hover:opacity-75 hover:scale-110 transition-transform duration-300"
         >
           SUPPORT US
         </Link>
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -8,9 +8,12 @@ export default function Home() {
       lang="en"
       className="overflow-x-hidden flex flex-col"
     >
-      <Navbar></Navbar>
+      <header className="z-50">
+        <Navbar />
+      </header>
+
       <main>
-        <FrontImage></FrontImage>
+        <Banner />
       </main>
 
       <footer></footer>
@@ -18,30 +21,30 @@ export default function Home() {
   );
 }
 
-function FrontImage() {
+function Banner() {
   return (
-    <div className="relative h-[702px] w-full flex flex-col">
-      <div>
-        <div className="flex flex-col items-center justify-center absolute w-full h-[700px] bg-[linear-gradient(70deg,_#000000_30%,_transparent)] z-10">
-          <div className="absolute left-[25px] z-20 text-[#ffd100] text-[3vw] font-extrabold px-4">
-            <p className="mt-[-10px] mb-[-10px]">We build our future</p>
-            <p className="mt-[-10px] mb-[-10px]">Brick by Brick!</p>
+    <section className="relative w-full flex flex-col">
+      <div className="relative h-[60vh] w-full">
+        <Image
+          src="/random.jpg"
+          alt="random"
+          fill
+          priority
+          className="object-cover z-[-1] opacity-50"
+        />
+        <div className="flex flex-col items-start justify-center h-full w-full bg-[linear-gradient(70deg,_#000000_30%,_transparent)] px-6">
+          <div className="text-[#ffd100] font-extrabold z-10 ml-[2vw]">
+            <p className="text-[4.5vh]/[5.5vh]">We build our future</p>
+            <p className="text-[4.5vh]/[5.5vh]">Brick by Brick!</p>
             <Link href="/support-us">
-              <button className="cursor-pointer flex items-center justify-center h-[3vw] w-[15vw] max-w-full bg-[linear-gradient(180deg,_#7D6800_0%,_#413400_50%)] text-[#ffd100] font-extrabold text-[1.25vw] rounded-[1.5vw] hover:opacity-75 border-2 border-[#ffd100] mt-[1vw]">
+              <button className="mt-4 bg-[linear-gradient(180deg,_#7D6800_0%,_#413400_50%)] text-[#ffd100] font-extrabold text-[2.25vh] px-[1vh] py-[0.5vh] rounded-[1.625vh]  border-[0.25vh] border-[#ffd100] hover:opacity-75 hover:scale-110 transition-transform duration-300">
                 BECOME A SPONSOR
               </button>
             </Link>
           </div>
         </div>
-        <Image
-          src="/random.jpg"
-          alt="random"
-          height={1280}
-          width={1920}
-          className="w-[1920px] h-[700px] object-cover z-[-1] opacity-50 ml-auto mr-[-150px]"
-        />
       </div>
-      <div className="w-full h-[2px] bg-[linear-gradient(90deg,_#000000_10%,_#ffd100_35%,_#000000_85%)] z-10 opacity-50"></div>
-    </div>
+      <div className="w-full h-[0.25vh] bg-[linear-gradient(90deg,_#000000_10%,_#ffd100_35%,_#000000_85%)] opacity-50" />
+    </section>
   );
 }
