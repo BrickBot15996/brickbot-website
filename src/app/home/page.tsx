7fdcacc4ce4166f8f8e8add0779f28a3d1ee29fa
mariusbaican <155394478+mariusbaican@users.noreply.github.com>
2025-05-24 00:58:54 +0300

    Added Small Projects

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -41,20 +41,7 @@ function Banner() {
           </div>
           <div className="ml-[3vw]">
             <Link href="/support-us">
-              <div className="">
-                <button className="flex flex-row justify-center space-x-[0.25vw] bg-[linear-gradient(180deg,_#7D6800_0%,_#413400_50%)] px-[1.25vw] py-[0.5vh] rounded-[2.5vh] border-[0.25vh] border-[#ffd100] hover:opacity-75 hover:scale-107 transition-transform duration-150 cursor-pointer">
-                  <p className="text-[#ffd100] font-extrabold text-[2.25vh]">
-                    BECOME A SPONSOR
-                  </p>
-                  <Image
-                    src="/right-arrow.png"
-                    alt="right-arrow"
-                    width={30}
-                    height={25}
-                    className="z-[10] inline mr-[-0.5vw]"
-                  />
-                </button>
-              </div>
+              <NavButton text="BECOME A SPONSOR" />
             </Link>
           </div>
         </div>
@@ -66,7 +53,7 @@ function Banner() {
 
 function AboutUs() {
   return (
-    <section className="relative flex flex-col items-start justify-center h-full w-full ml-[3vw] mt-[4vh] mb-[10vh]">
+    <section className="relative flex flex-col items-start justify-center h-full w-full px-[3vw] py-[5vh]">
       <h1 className="text-[#ffd100] font-extrabold text-[4vh] mb-[1vh]">
         About Us
       </h1>
@@ -87,29 +74,20 @@ function AboutUs() {
           </div>
         </div>
         <div className="relative rounded-[2.5vh] p-[0.25vh] bg-[linear-gradient(180deg,_#ffffff26,_#58585826)] w-[46vw] object-contain">
-          <Image
-            src="/random.jpg"
-            alt="random"
-            fill
-            className="object-cover rounded-[2.375vh] overflow-hidden"
-          />
+          <div className="relative w-full h-full rounded-[2.375vh] overflow-hidden">
+            <Image
+              src="/random.jpg"
+              alt="random"
+              fill
+              className="object-cover"
+            />
+          </div>
         </div>
       </div>
       <div className="mt-[2vh]">
         <Link href="/achievements">
           <div className="">
-            <button className="flex flex-row justify-center space-x-[0.25vw] bg-[linear-gradient(180deg,_#7D6800_0%,_#413400_50%)] px-[2vh] py-[0.5vh] rounded-[2.5vh] border-[0.25vh] border-[#ffd100] hover:opacity-75 hover:scale-107 transition-transform duration-150 cursor-pointer">
-              <p className="text-[#ffd100] font-extrabold text-[2.25vh]">
-                OUR ACHIEVEMENTS
-              </p>
-              <Image
-                src="/right-arrow.png"
-                alt="right-arrow"
-                width={30}
-                height={25}
-                className="z-[10] inline mr-[-0.25vw]"
-              />
-            </button>
+            <NavButton text="OUR ACHIEVEMENTS" />
           </div>
         </Link>
       </div>
@@ -119,24 +97,108 @@ function AboutUs() {
 
 function Projects() {
   return (
-    <section className="relative flex flex-col items-start justify-center h-full w-full ml-[3vw] mt-[2vh]">
-      <h1 className="text-[#ffd100] font-extrabold text-[4vh] mb-[1vh]">
+    <section className="relative flex flex-col items-start justify-center h-full w-full px-[3vw] py-[5vh]">
+      <h1 className="text-[#ffd100] font-extrabold text-[4vh] mb-[2vh]">
         Projects
       </h1>
-      <div className="flex flex-row">
-        <SmallProject />
-        <SmallProject />
-        <SmallProject />
-        <SmallProject />
+      <div className="grid grid-cols-4 gap-[8vw]">
+        <SmallProject
+          color="#FFD100"
+          name={"Portfolio\nLibrary"}
+          logoPath="/bricklogo.png"
+          link="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+        />
+        <SmallProject
+          color="#FF6750"
+          name={"BrickBot Guide &\nDocumentation"}
+          logoPath="/bricklogo.png"
+          link="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+        />
+        <SmallProject
+          color="#9E50FF"
+          name={"Connecting\nFTC Teams"}
+          logoPath="/bricklogo.png"
+          link="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+        />
+        <SmallProject
+          color="#ADFF50"
+          name={"România\nOnestă"}
+          logoPath="/bricklogo.png"
+          link="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+        />
       </div>
     </section>
   );
 }
 
-function SmallProject() {
+type ProjectProps = {
+  color: string;
+  name: string;
+  logoPath: string;
+  link: string;
+};
+
+function SmallProject({ color, name, logoPath, link }: ProjectProps) {
   return (
-    <div>
-      <div></div>
-    </div>
+    <Link href={link}>
+      <div className="relative rounded-[2.5vh] p-[0.25vh] bg-[linear-gradient(180deg,_#ffffff26,_#58585826)] min-h-[250px] h-[35vh] hover:opacity-75 hover:scale-105 transition-transform duration-150 cursor-pointer">
+        {/* this is so cursed */}
+        <div className="w-auto h-full rounded-[2.375vh] bg-[#0a0a0a]">
+          <div className="w-[17.5vw] h-full rounded-[2.375vh] bg-[linear-gradient(180deg,_#6363630D,_#0202020D)] px-[1vw] py-[2vh] min-h-[250px]">
+            <div
+              className="absolute left-0 top-0 rounded-[2.5vh] w-full h-full"
+              style={{
+                background: `linear-gradient(180deg, ${color}1B, #0202021B)`,
+              }}
+            ></div>
+            <div className="flex items-center justify-center w-full h-[23vh] overflow-hidden">
+              <Image
+                src={logoPath}
+                alt={name + "Logo"}
+                width={275}
+                height={275}
+                className="object-contain"
+              />
+            </div>
+            <div className="absolute bottom-[1vw] left-[1.25vw] max-w-[80%] min-h-[4.5vh]">
+              <p
+                className="font-medium text-[3vh]/[3vh]"
+                style={{ whiteSpace: "pre-line", color: color }}
+              >
+                {name}
+              </p>
+            </div>
+            <div className="absolute bottom-[1vw] right-[1vw]">
+              <Image
+                src="/right-arrow-white.png"
+                alt="right-arrow"
+                width={50}
+                height={50}
+                className="z-[10] inline mr-[-0.5vw]"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+type NavButtonProps = {
+  text: string;
+};
+
+function NavButton({ text }: NavButtonProps) {
+  return (
+    <button className="flex flex-row justify-center space-x-[0.25vw] bg-[linear-gradient(180deg,_#7D6800_0%,_#413400_50%)] px-[1.25vw] py-[0.5vh] rounded-[2.5vh] border-[0.25vh] border-[#ffd100] hover:opacity-75 hover:scale-107 transition-transform duration-150 cursor-pointer">
+      <p className="text-[#ffd100] font-extrabold text-[2.25vh]">{text}</p>
+      <Image
+        src="/right-arrow-yellow.png"
+        alt="right-arrow"
+        width={30}
+        height={25}
+        className="z-[10] inline mr-[-0.5vw]"
+      />
+    </button>
   );
 }
