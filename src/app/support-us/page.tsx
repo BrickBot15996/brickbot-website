ddfd0d1ba78e9cc38082516a63b02ed5fb8537db
mariusbaican <155394478+mariusbaican@users.noreply.github.com>
2025-06-20 01:47:28 +0300

    Improved expanding and retracting animation; Renamed donation section;

--- src/app/support-us/page.tsx
+++ src/app/support-us/page.tsx
@@ -58,50 +58,61 @@ function ContractSectionMobile() {
       <h4 className="text-[var(--alternate-text)] font-medium mr-auto text-[1.5rem] md:text-[1.75rem] lg:text-[2rem] mb-[1rem]">
         Sponsorship Tiers
       </h4>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-[1rem]">
-        <SponsorshipTierMobile
-          name="Bronze"
-          priceThreshold={100}
-          textColor="var(--bronze-tier-text)"
-          gradientColor="var(--bronze-tier-gradient)"
-          benefits={["Social Media Mentions", "Logo on Robot"]}
-        />
-        <SponsorshipTierMobile
-          name="Silver"
-          priceThreshold={250}
-          textColor="var(--silver-tier-text)"
-          gradientColor="var(--silver-tier-gradient)"
-          benefits={[
-            "Social Media Mentions",
-            "Logo on Robot",
-            "Logo on Website",
-          ]}
-        />
-        <SponsorshipTierMobile
-          name="Gold"
-          priceThreshold={750}
-          textColor="var(--gold-tier-text)"
-          gradientColor="var(--gold-tier-gradient)"
-          benefits={[
-            "Social Media Mentions",
-            "Logo on Robot",
-            "Logo on Website",
-            "Logo on Roll-Up",
-          ]}
-        />
-        <SponsorshipTierMobile
-          name="Diamond"
-          priceThreshold={1500}
-          textColor="var(--diamond-tier-text)"
-          gradientColor="var(--diamond-tier-gradient)"
-          benefits={[
-            "Social Media Mentions",
-            "Logo on Robot",
-            "Logo on Website",
-            "Logo on Roll-Up",
-            "Logo on shirts",
-          ]}
-        />
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-[1rem] items-start">
+        <div className="self-start">
+          <SponsorshipTierMobile
+            name="Bronze"
+            priceThreshold={100}
+            textColor="var(--bronze-tier-text)"
+            gradientColor="var(--bronze-tier-gradient)"
+            benefits={["Social Media Mentions", "Logo on Robot"]}
+          />
+        </div>
+        <div className="self-start">
+          <SponsorshipTierMobile
+            name="Silver"
+            priceThreshold={250}
+            textColor="var(--silver-tier-text)"
+            gradientColor="var(--silver-tier-gradient)"
+            benefits={[
+              "Social Media Mentions",
+              "Logo on Robot",
+              "Logo on Website",
+            ]}
+            className="self-start"
+          />
+        </div>
+        <div className="self-start">
+          <SponsorshipTierMobile
+            name="Gold"
+            priceThreshold={750}
+            textColor="var(--gold-tier-text)"
+            gradientColor="var(--gold-tier-gradient)"
+            benefits={[
+              "Social Media Mentions",
+              "Logo on Robot",
+              "Logo on Website",
+              "Logo on Roll-Up",
+            ]}
+            className="self-start"
+          />
+        </div>
+        <div className="self-start">
+          <SponsorshipTierMobile
+            name="Diamond"
+            priceThreshold={1500}
+            textColor="var(--diamond-tier-text)"
+            gradientColor="var(--diamond-tier-gradient)"
+            benefits={[
+              "Social Media Mentions",
+              "Logo on Robot",
+              "Logo on Website",
+              "Logo on Roll-Up",
+              "Logo on shirts",
+            ]}
+            className="self-start"
+          />
+        </div>
         <div className="lg:col-span-2 lg:px-[25%]">
           <SponsorshipTierMobile
             name="Ultimate"
@@ -140,7 +151,7 @@ function ContractSectionDesktop() {
           height: isExpanded ? "25rem" : "13rem",
         }}
       >
-        <div className="grid grid-cols-5 w-[93%] h-full gap-x-[1rem]">
+        <div className="grid grid-cols-5 w-[93%] h-full gap-[1rem]">
           <SponsorshipTierDesktop
             name="Bronze"
             priceThreshold={100}
@@ -289,49 +300,64 @@ function SponsorshipTierMobile({
       gradient={true}
       color={gradientColor}
       borderRadius="1.3rem"
-      className={`flex flex-col w-full select-none transition-all duration-400 p-[1rem] cursor-pointer
-    ${isExpanded ? "max-h-[100rem]" : "max-h-[3.65rem] md:max-h-[3.9rem]"}
-  `}
+      className="flex flex-col w-full select-none p-[1rem] cursor-pointer overflow-hidden"
       action={() => {
         setExpanded(!isExpanded);
       }}
     >
-      <div className="flex flex-col items-center justify-center">
-        <div className="grid grid-cols-3 h-full w-full items-center justify-center mx-[0.5rem] mb-[0.5rem]">
-          <h5
-            className="mr-auto font-semibold text-[1.25rem]/[1.25rem] md:text-[1.5rem]/[1.5rem]"
-            style={{ color: textColor }}
-          >
-            {name}
-          </h5>
-          <h5
-            className="font-semibold text-[1.25rem]/[1.25rem] md:text-[1.5rem]/[1.5rem] ml-auto"
-            style={{ color: textColor }}
-          >
-            ≥ {priceThreshold} EUR
-          </h5>
-          <motion.div
-            animate={{ rotate: isExpanded ? 180 : 0 }}
-            transition={{ duration: 0.4 }}
-            className="flex items-center justify-center ml-auto h-[1.75rem] w-[1.75rem]"
-          >
-            <IoIosArrowDown
-              className="h-full w-full"
-              style={{ color: textColor }}
-            />
-          </motion.div>
-        </div>
-        <ul
-          className="pl-[2rem] mt-[1rem] list-disc transition-all duration-400 font-medium text-[1.1rem]/[1.35rem] md:text-[1.25rem]/[1.5rem] mb-[1rem] mr-auto"
-          style={{
-            opacity: isExpanded ? "100" : "0",
-          }}
+      <div className="flex items-center justify-between w-full min-h-[3rem]">
+        <h5
+          className="font-semibold text-[1.25rem]/[1.25rem] md:text-[1.5rem]/[1.5rem]"
+          style={{ color: textColor }}
         >
-          {benefits.map((benefit, index) => {
-            return <li key={index}>{benefit}</li>;
-          })}
-        </ul>
+          {name}
+        </h5>
+
+        <h5
+          className="font-semibold text-[1.25rem]/[1.25rem] md:text-[1.5rem]/[1.5rem]"
+          style={{ color: textColor }}
+        >
+          ≥ {priceThreshold} EUR
+        </h5>
+
+        <motion.div
+          animate={{ rotate: isExpanded ? 180 : 0 }}
+          transition={{ duration: 0.3, ease: "easeInOut" }}
+          className="flex items-center justify-center h-[1.75rem] w-[1.75rem]"
+        >
+          <IoIosArrowDown
+            className="h-full w-full"
+            style={{ color: textColor }}
+          />
+        </motion.div>
       </div>
+
+      <motion.div
+        initial={false}
+        animate={{
+          height: isExpanded ? "auto" : 0,
+          opacity: isExpanded ? 1 : 0,
+        }}
+        transition={{
+          duration: 0.3,
+          ease: "easeInOut",
+          opacity: {
+            duration: isExpanded ? 0.3 : 0.3,
+          },
+        }}
+        className="overflow-hidden"
+      >
+        <ul className="pt-[1rem] pl-[2rem] list-disc font-medium text-[1.1rem]/[1.35rem] md:text-[1.25rem]/[1.5rem] text-[var(--alternate-text)]">
+          {benefits.map((benefit, index) => (
+            <li
+              key={index}
+              className="mb-[0.25rem] last:mb-0"
+            >
+              {benefit}
+            </li>
+          ))}
+        </ul>
+      </motion.div>
     </Box>
   );
 }
@@ -352,6 +378,9 @@ function DonationSection() {
           </p>
           <Button
             text="COMPLETE THE FORM"
+            action={() => {
+              window.open("https://formular230.ro/", "_blank");
+            }}
             arrow={true}
             accentColor="var(--default-yellow)"
             gradientColorLight="var(--yellow-gradient-light)"
@@ -361,7 +390,7 @@ function DonationSection() {
 
         <div className="flex flex-col w-full lg:w-1/2 gap-y-[2rem] md:gap-y-[2.5rem] lg:gap-y-[3rem]">
           <h1 className="text-[var(--default-yellow)] font-bold text-[2.75rem]/[3rem] md:text-[3.25rem]/[3.5rem] lg:text-[3.75rem]/[4rem]">
-            Collaboration
+            Donation
           </h1>
           <p className="text-[1.1rem] md:text-[1.5rem] xl:text-[1.75rem] font-medium text-[var(--default-text)]">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
@@ -370,7 +399,13 @@ function DonationSection() {
             quibusdam facilis eaque facere.
           </p>
           <Button
-            text="COMPLETE THE FORM"
+            text="DONATE HERE"
+            action={() => {
+              window.open(
+                "https://support.stripe.com/questions/how-to-accept-donations-through-stripe",
+                "_blank"
+              );
+            }}
             arrow={true}
             accentColor="var(--default-yellow)"
             gradientColorLight="var(--yellow-gradient-light)"
